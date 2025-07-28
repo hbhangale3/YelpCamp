77cec53a2c5337a26616fe1b0702ffd4844a8df9
hbhangale3 <hbhangale3@gmail.com>
2025-07-27 18:38:24 -0700

    using ejs-mate to enhance code reusability

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const { render } = require('ejs');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 .then(()=>{
@@ -15,7 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     console.log(err);
 })
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}))
